794431d4a7763d5b230a0e3ef1b369e23244abc1
Belu Camandone <86261762+bcamandone@users.noreply.github.com>
2024-01-21 14:51:10 -0300

    Update Resolución.sql

--- 8 Week SQL Challenge/Case_Study_4/A. Exploración de nodos de clientes/Resolución.sql
+++ 8 Week SQL Challenge/Case_Study_4/A. Exploración de nodos de clientes/Resolución.sql
@@ -17,3 +17,29 @@ JOIN regions r
 ON n.region_id = r.region_id
 GROUP BY 1,2
 ORDER BY 1 
+
+--4) ¿En cuántos días en promedio se reasignan los clientes a un nodo diferente?
+WITH dias AS (
+  SELECT 
+    customer_id,
+    region_id,
+    node_id,
+    MIN(start_date) AS first_date --por cliente, región y nodo busco la 1er fecha
+  FROM customer_nodes
+  GROUP BY customer_id, region_id, node_id
+),
+movimientos AS (
+  SELECT
+    customer_id,
+    node_id,
+    region_id,
+    first_date,
+   (LEAD(first_date) OVER(PARTITION BY customer_id ORDER BY first_date))  - (first_date) as dias_movimiento
+  FROM dias
+)
+
+SELECT 
+  ROUND(AVG(dias_movimiento),0) AS promedio_de_dias
+FROM movimientos;
+
+
