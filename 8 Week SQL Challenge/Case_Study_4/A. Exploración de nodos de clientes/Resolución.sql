d28e6d36e425f97305fc4ddcac3d9a2f7a8f5a6f
Belu Camandone <86261762+bcamandone@users.noreply.github.com>
2024-01-21 15:19:01 -0300

    Update Resolución.sql

--- 8 Week SQL Challenge/Case_Study_4/A. Exploración de nodos de clientes/Resolución.sql
+++ 8 Week SQL Challenge/Case_Study_4/A. Exploración de nodos de clientes/Resolución.sql
@@ -42,4 +42,35 @@ SELECT
   ROUND(AVG(dias_movimiento),0) AS promedio_de_dias
 FROM movimientos;
 
+--5)¿Cuál es la mediana, percentil 80 y 95 para esta misma métrica de días de reasignación para cada región?
+WITH dias AS (
+  SELECT 
+    customer_id,
+    r.region_id,
+	r.region_name,
+    node_id,
+    MIN(start_date) AS first_date --por cliente, región y nodo busco la 1er fecha
+  FROM customer_nodes c
+  JOIN regions r 
+  ON c.region_id = r.region_id
+  GROUP BY customer_id, r.region_id, node_id,r.region_name
+),
+movimientos AS (
+  SELECT
+    customer_id,
+    node_id,
+    region_id,
+	region_name,
+    first_date,
+   (LEAD(first_date) OVER(PARTITION BY  region_id  ORDER BY first_date))  - (first_date) as dias_movimiento
+  FROM dias
+)
 
+SELECT 
+  region_id,
+  region_name,
+  PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY dias_movimiento) OVER(PARTITION BY region_id)  AS mediana,
+  PERCENTILE_CONT(0.8) WITHIN GROUP (ORDER BY  dias_movimiento) OVER(PARTITION BY region_id)  AS percentil_80,
+  PERCENTILE_CONT(0.95) WITHIN GROUP (ORDER BY dias_movimiento)OVER(PARTITION BY region_id)  AS percentil_95
+FROM movimientos
+ORDER BY 1
