90c55319734106e8a796e76bf9ef34917ee9cca9
Belu Camandone <86261762+bcamandone@users.noreply.github.com>
2024-01-21 15:59:08 -0300

    Update Resolución.sql

--- 8 Week SQL Challenge/Case_Study_4/B. Transacciones de clientes/Resolución.sql
+++ 8 Week SQL Challenge/Case_Study_4/B. Transacciones de clientes/Resolución.sql
@@ -22,3 +22,23 @@ SELECT
   ROUND(AVG(cuenta),0) AS promedio_casos,
   ROUND(AVG(total),2) AS promedio_montos
 FROM depositos
+
+--3)Para cada mes, ¿cuántos clientes de Data Bank realizan más de 1 depósito y 1 compra o 1 retiro en un solo mes?
+WITH transacciones AS (
+  SELECT 
+    customer_id,
+    EXTRACT(MONTH FROM txn_date) AS mes,
+    SUM(CASE WHEN txn_type = 'deposit' THEN 1 ELSE 0 END) AS depositos,
+    SUM(CASE WHEN txn_type = 'purchase' THEN 1 ELSE 0 END) AS compra,
+    SUM(CASE WHEN txn_type = 'withdrawal' THEN 1 ELSE 0 END) AS retiro
+  FROM customer_transactions
+  GROUP BY customer_id,  EXTRACT(MONTH FROM txn_date)
+)
+
+SELECT 
+  mes,
+  COUNT(customer_id) AS clientes
+FROM transacciones
+WHERE depositos > 1
+  AND (compra = 1 OR retiro = 1)
+GROUP BY mes;
