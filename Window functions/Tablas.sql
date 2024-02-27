8732b297a40a1e45ed0d88d97c75a8038a197b00
Belu Camandone <86261762+bcamandone@users.noreply.github.com>
2024-02-27 00:42:37 -0300

    Create Tablas.sql

---
+++ Window functions/Tablas.sql
@@ -0,0 +1,33 @@
+CREATE TABLE product_groups (
+	group_id serial PRIMARY KEY,
+	group_name VARCHAR (255) NOT NULL
+);
+
+CREATE TABLE products (
+	product_id serial PRIMARY KEY,
+	product_name VARCHAR (255) NOT NULL,
+	price DECIMAL (11, 2),
+	group_id INT NOT NULL,
+	FOREIGN KEY (group_id) REFERENCES product_groups (group_id)
+);
+
+
+INSERT INTO product_groups (group_name)
+VALUES
+	('Smartphone'),
+	('Laptop'),
+	('Tablet');
+
+INSERT INTO products (product_name, group_id,price)
+VALUES
+	('Microsoft Lumia', 1, 200),
+	('HTC One', 1, 400),
+	('Nexus', 1, 500),
+	('iPhone', 1, 900),
+	('HP Elite', 2, 1200),
+	('Lenovo Thinkpad', 2, 700),
+	('Sony VAIO', 2, 700),
+	('Dell Vostro', 2, 800),
+	('iPad', 3, 700),
+	('Kindle Fire', 3, 150),
+	('Samsung Galaxy Tab', 3, 200);
