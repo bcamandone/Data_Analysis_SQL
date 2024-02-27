9d56aec926ae41f763755591dc57aed3992e574e
Belu Camandone <86261762+bcamandone@users.noreply.github.com>
2024-02-27 01:15:36 -0300

    Update Conjuntas de ejemplo.sql

--- Window functions/Conjuntas de ejemplo.sql
+++ Window functions/Conjuntas de ejemplo.sql
@@ -22,3 +22,18 @@ SELECT
 FROM
 	products p 
 	INNER JOIN product_groups pg ON p.group_id = pg.group_id;
+
+--3) Ejemplo con FIRST_VALUE 
+--La siguiente consulta utiliza FIRST_VALUE() para devolver el precio más bajo para cada grupo de productos.
+
+SELECT
+	product_name,
+	group_name,
+	price,
+	FIRST_VALUE (price) OVER (
+		PARTITION BY group_name
+		ORDER BY
+			price) AS menor_precio_por_grupo
+FROM
+	products p 
+	INNER JOIN product_groups pg ON p.group_id = pg.group_id;
