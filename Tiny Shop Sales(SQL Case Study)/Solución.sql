94669ddc97431df947fe8aa6495d068ffa204f3e
Belu Camandone <86261762+bcamandone@users.noreply.github.com>
2024-05-13 13:37:46 -0300

    Update Solución.sql

--- Tiny Shop Sales(SQL Case Study)/Solución.sql
+++ Tiny Shop Sales(SQL Case Study)/Solución.sql
@@ -18,3 +18,23 @@ FROM ordenes)
 SELECT name
 FROM top
 WHERE rnk=1
+
+--3)¿Cuál es el ingreso total por producto?
+
+SELECT p.product_id,product_name,SUM(price*quantity) AS ingreso
+FROM products_tiny p 
+JOIN order_items o 
+ON p.product_id=o.product_id
+GROUP BY  1,2
+ORDER BY ingreso DESC
+
+--4)Encuentra el día con mayores ingresos.
+
+SELECT order_date,COUNT(o.order_id) AS ordenes,SUM(price*quantity) AS ingreso
+FROM orders o 
+JOIN order_items oi 
+ON o.order_id=oi.order_id
+JOIN products_tiny p 
+ON p.product_id=oi.product_id
+GROUP BY order_date
+ORDER BY ingreso DESC, ordenes DESC
